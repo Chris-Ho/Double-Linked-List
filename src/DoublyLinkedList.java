638d5f40d7aa7f174367f6bcb3a02f746e706a05
ChrisHo02 <61057558+ChrisHo02@users.noreply.github.com>
2021-09-10 13:38:22 +0100

    Add files via upload

---
+++ src/DoublyLinkedList.java
@@ -0,0 +1,87 @@
+public class DoublyLinkedList {
+    private ListNode2 head = null;
+    private int n = 0; // list size
+
+    public void addFirst(Object o) {
+
+        head = new ListNode2(o, null, head);
+        if(head.next != null){
+            head.next.prev = head;
+        }
+        n++;
+    }
+
+    public Object get(int i){
+        ListNode2 node = head;
+        for(int j=0;j<i; j++){
+            node = node.next;
+        }
+        return node.element;
+    }
+
+    public void insert(Object o, int i) {
+        if(i==0){
+            addFirst(o);
+            return;
+        }
+        ListNode2 node = head;
+        for(int j = 0; j<i-1; j++){
+            node = node.next;
+        }
+        node.next = new ListNode2(o, node,node.next);
+        if(node.next.next!=null){
+            node.next.next.prev = node.next;
+        }
+        n++;
+    }
+
+    public void remove(int i) {
+        if(i==0){
+            head = head.next;
+            n--;
+            if(head != null){
+                head.prev = null;
+            }
+        }
+        else{
+            ListNode2 node = head;
+            for(int j = 0; j<i-1; j++){
+                node = node.next;
+            }
+            node.next = node.next.next;
+            if(node.next != null){
+                node.next.prev = node;
+            }
+            n--;
+        }
+    }
+
+    /**
+     * Prints out the elements in the list from the first to the last (front to back) and then from the last to the first
+     * (back to front). This is useful to test whether the list nodes are connected correctly with next and prev references.
+     */
+    public void print() {
+        // no elements to print for empty list
+        if (head == null) {
+            System.out.println("list empty.");
+            return;
+        }
+
+        // follow next references to list elements from the front to the back of the list
+        System.out.print("front to back: ");
+        ListNode2 node = head;
+        System.out.print(node.element + " ");
+        while (node.next != null) {
+            node = node.next;
+            System.out.print(node.element + " ");
+        }
+
+        // follow prev references to list elements from the back to the front of the list
+        System.out.print("-- and back to front: ");
+        while (node != null) {
+            System.out.print(node.element + " ");
+            node = node.prev;
+        }
+        System.out.println();
+    }
+}
